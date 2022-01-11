e7b7902d6e74fb189103f6a74fc25b9b120d7e26
Maksim Dudarev <maxim.d132213@gmail.com>
2022-01-11 11:08:49 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.scss';
+import NewCopmonent from './components/NewComponent';
+
+const  App = () => {
+  return (
+    <div className="App">
+        <NewCopmonent/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/NewComponent.jsx
@@ -0,0 +1,18 @@
+import styles from './NewComponent.module.scss'
+
+
+const NewCopmonent = () => {
+    return (
+        <div className={styles.one}>
+            text
+            <div className={styles.two}>
+                text 2
+            </div>
+            <div className={styles.three}>
+                text 3
+            </div>
+        </div>
+    )
+}
+
+export default NewCopmonent;
